150d5b66f61cbbcbcfd014cf028e0d6ff95ecda0
Louis Savoie <thelousavoie@gmail.com>
2021-08-01 15:56:51 -0400

    add: zed component skeleton

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Player from "./components/Player";
 import House from "./components/House";
+import Zed from "./components/Zed";
 
 function App() {
   return (
     <div className="App">
       <h1>Zed Reaction</h1>
+      <Zed />
       <House />
       <Player />
     </div>
---
+++ src/components/Zed.js
@@ -0,0 +1,22 @@
+function Zed() {
+  return (
+    <div className="zed">
+      <div id="zed" className="info-frame">
+        <h3>Zed</h3>
+        <div id="zed-here" className="list">
+          <div className="item">
+            <p>Carl</p>
+          </div>
+          <div className="item">
+            <p>Bonnie</p>
+          </div>
+          <div className="item">
+            <p>Walter</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Zed;
