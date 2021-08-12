2c194b0a0b00e452736c245d28a1008018adfff5
Louis Savoie <thelousavoie@gmail.com>
2021-08-12 13:51:02 -0400

    add app states

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
+import { useState } from 'react'
 import Player from "./components/Player";
 import House from "./components/House";
 import ZedHere from "./components/ZedHere";
 
 function App() {
+
+  const [houses, setHouses] = useState({})
+  const [zed, setZed] = useState({})
+  const [map, setMap] = useState([[{}]])
+
   return (
     <div id="app" className="container">
       <h1>Zed Reaction</h1>
